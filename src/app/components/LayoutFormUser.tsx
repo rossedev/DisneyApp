2f57494239ad50f380e3522cde80bd05dc06f4a9
Rose <rosa.morales@growproexperience.com>
2024-03-11 16:46:36 -0500

    fix login

--- src/app/components/LayoutFormUser.tsx
+++ src/app/components/LayoutFormUser.tsx
@@ -6,12 +6,12 @@ export const LayoutFormUser = (props: any) => {
   return (
     <div className="w-full animate-fade animate-ease-in-out animate-duration-400 backdrop-blur-0 lg:backdrop-blur-xl border rounded-md">
       <div className="flex flex-col justify-center px-4 py-8">
-        <form onClick={onSubmit}>
           <label className="text-sm font-bold text-center mb-4">{title}</label>
           {children}
           <button
             type="submit"
             className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={onSubmit}
           >
             {loading && <Loading />}
             {nameButton}
@@ -22,7 +22,6 @@ export const LayoutFormUser = (props: any) => {
               {errors.general}
             </p>
           )}
-        </form>
       </div>
     </div>
   );
